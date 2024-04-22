abf25a18733a029663d602e7b8f5dfe7fe7dcdbd
Luciano Arrúa <lucianorarrua@gmail.com>
2024-04-22 20:28:42 -0300

    Add CRA packages

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    // Obtener la URL actual con todos los parámetros de consulta
+    const urlActual = window.location.href;
+
+    // Reemplaza "URL_DE_DESTINO" con la URL a la que desees redirigir
+    const urlDestino = 'http://localhost:5173/mp-auth';
+
+    const href =
+      urlDestino +
+      (urlActual.includes('?')
+        ? urlActual.substring(urlActual.indexOf('?'))
+        : '');
+
+    // Redireccionar automáticamente a la URL de destino incluyendo los parámetros de consulta
+    window.location.href = href;
+  }, []);
+
+  return <>Redireccionando...</>;
+}
+
+export default App;
